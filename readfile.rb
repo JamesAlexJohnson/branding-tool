2bc3fdb8dfd970724f82c59e0d69265fb0d228b4
James Johnson <james.johnson@granicus.com>
2016-08-16 11:47:41 -0600

    everything seems to be working

--- readfile.rb
+++ readfile.rb
@@ -1,5 +1,5 @@
 #!/bin/ruby
-
+require_relative 'templates/style_templates'
 class ReadFile 
 
 @path_to_file 
@@ -7,80 +7,59 @@ class ReadFile
 @file
 @file_out
 @project_name = "civica-mobile"
-@r
+@ins
+@main_color
+@text_in_from_template
 
 	def initialize
-		@r = 
-"(function() {
-    crm.ui.colors = {
-            labelColor: '#000000',
-            mainColor: '#6a1a41',
-            accent: '#380e22', 
-            btnPosFontColor: '#000000',
-            btnBorderColor: '#000000',
-            btnPosBgColor: '#380e22',
-            bgColor: '#ffffff',
-            calendarViewColor: '#ffffff'
-    };
-    
-    crm.ui.theme = {
-            winBgColor:'#f0f0f0',
-            winTopBgColor:'#06385a',
-            winBottomBgColor:'#ffffff',
-            textColor:'#282828',
-            textBGColor: '#fff',
-            labelColor:'#000000',
-            grayTextColor:'#888888',
-            headerColor:'#FFFFFF',
-            lightBlue:'#006cb1',
-            darkBlue:'#93caed',
-            bgNavActive:'#ffffff',
-            bgRowOdd: '#26364e',
-            labelColor: '#A9A9A9',
-            bgNavNotActive:'#222222',
-            fontFamily:crm.os({
-                    iphone:'Helvetica Neue',
-                    android:'Droid Sans'
-            }),
-    };
-
-})();
-"
-
-		puts "initialize"
-		pull_repo(@project_name)
-		get_path
-		read_in
-		edit_file
+		greeting
+		file = read_in(setup)
+		styles = Styles.new
+		@text_in_from_template = styles.get_template(1)
+		puts @text_in_from_template
+		edit_file(file)
 	end
 
-	def pull_repo(project_name)
-		puts "pull repo"
-
-		# enter git credentials		
-		# pull down project 
+	def greeting
+		puts "Welcome to the branding tool, press enter to continue \n\n"
+		input_value = gets
+		return unless !continue?(input_value, "\n")
+		puts "invalid entry \n\n"
+		greeting
 	end
 
-	def get_path
-		@path_to_file = "colors.js"
+	def setup
+		puts "please copy and past the full file path of the project to be edited\n\n"
+		file_path = gets.chomp
+		puts File.exist?(file_path)
+		return file_path unless !File.exist?(file_path)
+		puts "There doesn't seem to be a file here \n\n"
+		setup
 	end
 
-  def read_in
+  def read_in(file_path)
 		begin 
-			raise "File not found"  unless (@path_to_file.nil? || File.exists?(@path_to_file))
-			@file = File.read(@path_to_file)
-			puts @file
+			@file = File.read(file_path)
 		rescue => e 
-			puts e.message
+			puts e.message 
 		end
 	end	
 
-	def edit_file
-		puts @r
-		@file_out = File.open(@path_to_file, "w") 
-		@file_out.syswrite(@file.gsub(/(?<=\/\/start)[^*]+(?=\/\/end)/, "\n #{@r} \n"))
+	def edit_file(file)
+		@file_out = File.open("colors.js", "w") 
+		@file_out.syswrite(@file.gsub(/(?<=\/\/----start)[^*]+(?=\/\/end)/, "\n #{@text_in_from_template} \n"))
 		@file_out.close
 	end
+
+	def set_color
+		@main_color = "\'\#ffffff\'"
+	end
+
+	private 
+
+		def continue?(input, correct_value)
+			input == correct_value
+		end
 
 end
 
---
+++ templates/style_templates.rb
@@ -0,0 +1,53 @@
+#!/bin/ruby
+
+class Styles
+
+  def initialize
+    puts "Styles initialize"
+  end
+  
+  def get_template(template_name)
+      @template = color_template
+  end
+
+  private 
+    def color_template
+        "edited (function() {
+                edited
+                crm.ui.colors = {
+                        labelColor: '#000000',
+                        mainColor: '#ffffff',
+                        accent: '#380e22', 
+                        btnPosFontColor: '#000000',
+                        btnBorderColor: '#000000',
+                        btnPosBgColor: '#380e22',
+                        bgColor: '#ffffff',
+                        calendarViewColor: '#ffffff'
+                };
+                
+                crm.ui.theme = {
+                        winBgColor:'#f0f0f0',
+                        winTopBgColor:'#06385a',
+                        winBottomBgColor:'#ffffff',
+                        textColor:'#282828',
+                        textBGColor: '#fff',
+                        labelColor:'#000000',
+                        grayTextColor:'#888888',
+                        headerColor:'#FFFFFF',
+                        lightBlue:'#006cb1',
+                        darkBlue:'#93caed',
+                        bgNavActive:'#ffffff',
+                        bgRowOdd: '#26364e',
+                        labelColor: '#A9A9A9',
+                        bgNavNotActive:'#222222',
+                        fontFamily:crm.os({
+                                iphone:'Helvetica Neue',
+                                android:'Droid Sans'
+                        }),
+                };
+
+            })();       
+        "
+    end
+
+end
